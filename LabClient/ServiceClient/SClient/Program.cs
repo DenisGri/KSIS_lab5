34f408645dfd54a3da4cfbe055e0d6d39bc68b60
DenisGri <42544087+DenisGri@users.noreply.github.com>
2021-04-13 17:28:05 +0300

    Add files via upload

---
+++ LabClient/ServiceClient/SClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using LabService;
+
+namespace SClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+
+            using var call = client.SayHelloStream();
+
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var response in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(response.Message);
+                    Console.WriteLine(response.MessageBox);
+                }
+            });
+
+            while (true)
+            {
+                var result = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(result))
+                {
+                    break;
+                }
+
+                await call.RequestStream.WriteAsync(new HelloRequest() {Name = result});
+            }
+
+            await call.RequestStream.CompleteAsync();
+            await readTask;
+            Console.ReadKey();
+        }
+    }
+}
